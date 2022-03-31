4006e891b730393e81cd1c3e71758fa39b5268f1
EhsanTgv <EhsanTgv@gmail.com>
2022-03-31 21:02:18 +0300

    N-to-M Relationships with Room

---
+++ app/src/main/java/com/taghavi/multipleroomtables/entities/relations/SubjectWithStudents.kt
@@ -0,0 +1,17 @@
+package com.taghavi.multipleroomtables.entities.relations
+
+import androidx.room.Embedded
+import androidx.room.Junction
+import androidx.room.Relation
+import com.taghavi.multipleroomtables.entities.Student
+import com.taghavi.multipleroomtables.entities.Subject
+
+data class SubjectWithStudents(
+    @Embedded val subject: Subject,
+    @Relation(
+        parentColumn = "subjectName",
+        entityColumn = "studentName",
+        associateBy = Junction(StudentSubjectCrossRef::class)
+    )
+    val students: List<Student>
+)
